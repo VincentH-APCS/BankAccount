2d19ba8a76a5382b70e45cd4c658eea4330390d3
VincentH-APCS <vhuang10@stuy.edu>
2020-10-27 21:18:23 -0400

    Tested the deposit method and the 'deposit success/failure' commnent addition

--- Tester.java
+++ Tester.java
@@ -2,6 +2,13 @@ public class Tester{
   public static void main(String[] args){
     BankAccount Ba1 = new BankAccount(11122233, "secureconnection");
     System.out.println(Ba1.getBalance());
-    System.out.println(Ba1.setPassword("newthinghaha"));
+    Ba1.setPassword("newthinghaha");
+    System.out.println(Ba1.deposit(500.01));
+    if( Ba1.deposit(500.01) ){
+           System.out.println("Deposit success!");
+      }else{
+           System.out.println("Deposit failure");
+      }
+    System.out.println(Ba1.getBalance());
   }
 }
