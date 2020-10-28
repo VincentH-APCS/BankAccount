9b61a40e810b2f6eeca01769d8427ed892c1d807
VincentH-APCS <vhuang10@stuy.edu>
2020-10-27 21:27:27 -0400

    Tested when withdraw amount is 0

--- Tester.java
+++ Tester.java
@@ -4,12 +4,12 @@ public class Tester{
     System.out.println(Ba1.getBalance());
     Ba1.setPassword("newthinghaha");
     //System.out.println(Ba1.deposit(500.01));
-    if( Ba1.deposit(0) ){
+    if( Ba1.deposit(555.05) ){
            System.out.println("Deposit Success!");
       }else{
            System.out.println("Deposit Failure");
       }
-      if( Ba1.withdraw(550.05) ){
+      if( Ba1.withdraw(0) ){
              System.out.println("Withdrawal Success!");
         }else{
              System.out.println("Withdrawal Failure");
