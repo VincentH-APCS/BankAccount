d08d9cd4098aca5a976de1e273495fc96117a2a8
VincentH-APCS <vhuang10@stuy.edu>
2020-10-27 21:23:03 -0400

    First test of withdraw

--- Tester.java
+++ Tester.java
@@ -5,10 +5,15 @@ public class Tester{
     Ba1.setPassword("newthinghaha");
     //System.out.println(Ba1.deposit(500.01));
     if( Ba1.deposit(-555) ){
-           System.out.println("Deposit success!");
+           System.out.println("Deposit Success!");
       }else{
-           System.out.println("Deposit failure");
+           System.out.println("Deposit Failure");
       }
+      if( Ba1.withdraw(-550.05) ){
+             System.out.println("Withdrawal Success!");
+        }else{
+             System.out.println("Withdrawal Failure");
+        }
     System.out.println(Ba1.getBalance());
   }
 }
