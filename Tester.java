a45307ec978e4f0e6373f1247d65d6deb5add250
VincentH-APCS <vhuang10@stuy.edu>
2020-10-27 21:19:05 -0400

    fixed it so it only deposits the amount once

--- Tester.java
+++ Tester.java
@@ -3,7 +3,7 @@ public class Tester{
     BankAccount Ba1 = new BankAccount(11122233, "secureconnection");
     System.out.println(Ba1.getBalance());
     Ba1.setPassword("newthinghaha");
-    System.out.println(Ba1.deposit(500.01));
+    //System.out.println(Ba1.deposit(500.01));
     if( Ba1.deposit(500.01) ){
            System.out.println("Deposit success!");
       }else{
