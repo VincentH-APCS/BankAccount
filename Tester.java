890343f53f3e6a16ec4b2bc4afd74df892ae15ad
VincentH-APCS <vhuang10@stuy.edu>
2020-10-27 22:15:56 -0400

    Last test of Steven Lei's test cases

--- Tester.java
+++ Tester.java
@@ -20,17 +20,10 @@ public class Tester{
 public static void main (String[] args) {
     BankAccount testAcct = new BankAccount(95, "somepassword");
 
-    // tests for withdraw()
-    System.out.println(testAcct.withdraw(-5));      // false because can't withdraw negative amount
-    System.out.println(testAcct.withdraw(0));       // true since you can deposit 0
-    System.out.println(testAcct.withdraw(4));       // false since current balance is 0, can't withdraw more than balance
-    System.out.println(testAcct.deposit(45.5));     // true
-    System.out.println(testAcct.withdraw(42));      // true
-    //
 
     // tests for toString()
-    //System.out.println(testAcct.deposit(3.5));      // true
-    //System.out.println(testAcct.getBalance());      // return 3.5
-    //System.out.println(testAcct.toString());        // return 95    3.5 (the space is a tab)
+    System.out.println(testAcct.deposit(3.5));      // true
+    System.out.println(testAcct.getBalance());      // return 3.5
+    System.out.println(testAcct.toString());        // return 95    3.5 (the space is a tab)
     //
   }}
