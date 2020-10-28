0de86a290082d0873822a0c3d4783b6e9b495c2a
VincentH-APCS <vhuang10@stuy.edu>
2020-10-27 22:17:15 -0400

    One final test of my own test cases-and it should be good

--- Tester.java
+++ Tester.java
@@ -1,29 +1,19 @@
 public class Tester{
-//  public static void main(String[] args){
-  //  BankAccount Ba1 = new BankAccount(11122233, "secureconnection");
-    //System.out.println(Ba1.getBalance());
-  // Ba1.setPassword("newthinghaha");
-  //  if( Ba1.deposit(555.05) ){
-      //    System.out.println("Deposit Success!");
-    //  }else{
-      //     System.out.println("Deposit Failure");
-      //}
-    //  if( Ba1.withdraw(7777) ){
-    //         System.out.println("Withdrawal Success!");
-    //    }else{
-    //         System.out.println("Withdrawal Failure");
-  //      }
-//    System.out.println(Ba1.getBalance());
-   //System.out.println(Ba1.toString());
-//}
-//}
-public static void main (String[] args) {
-    BankAccount testAcct = new BankAccount(95, "somepassword");
-
-
-    // tests for toString()
-    System.out.println(testAcct.deposit(3.5));      // true
-    System.out.println(testAcct.getBalance());      // return 3.5
-    System.out.println(testAcct.toString());        // return 95    3.5 (the space is a tab)
-    //
-  }}
+  public static void main(String[] args){
+    BankAccount Ba1 = new BankAccount(11122233, "secureconnection");
+    System.out.println(Ba1.getBalance());
+   Ba1.setPassword("newthinghaha");
+    if( Ba1.deposit(555.05) ){
+         System.out.println("Deposit Success!");
+      }else{
+           System.out.println("Deposit Failure");
+      }
+     if( Ba1.withdraw(7777) ){
+            System.out.println("Withdrawal Success!");
+       }else{
+            System.out.println("Withdrawal Failure");
+        }
+    System.out.println(Ba1.getBalance());
+   System.out.println(Ba1.toString());
+}
+}
