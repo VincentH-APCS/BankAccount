3c3a55717bea0bd8023fb25444f74d65e04d69a8
VincentH-APCS <vhuang10@stuy.edu>
2020-10-27 21:05:43 -0400

    Test to see if the setPassword method worked or not

--- Tester.java
+++ Tester.java
@@ -2,6 +2,6 @@ public class Tester{
   public static void main(String[] args){
     BankAccount Ba1 = new BankAccount(11122233, "secureconnection");
     System.out.println(Ba1.getBalance());
-    System.out.println(Ba1.getAccountID());
+    System.out.println(Ba1.setPassword("newthinghaha"));
   }
 }
