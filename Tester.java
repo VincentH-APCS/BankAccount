ea6f0d7a860240c77f72d9987fc87f43b7e926e3
VincentH-APCS <vhuang10@stuy.edu>
2020-10-27 22:05:25 -0400

    Tested the test cases of Christopher Liu

--- Tester.java
+++ Tester.java
@@ -1,19 +1,79 @@
-public class Tester{
-  public static void main(String[] args){
-    BankAccount Ba1 = new BankAccount(11122233, "secureconnection");
-    System.out.println(Ba1.getBalance());
-    Ba1.setPassword("newthinghaha");
-    //System.out.println(Ba1.deposit(500.01));
-    if( Ba1.deposit(555.05) ){
-           System.out.println("Deposit Success!");
-      }else{
-           System.out.println("Deposit Failure");
-      }
-      if( Ba1.withdraw(7777) ){
-             System.out.println("Withdrawal Success!");
-        }else{
-             System.out.println("Withdrawal Failure");
+//public class Tester{
+//  public static void main(String[] args){
+  //  BankAccount Ba1 = new BankAccount(11122233, "secureconnection");
+    //System.out.println(Ba1.getBalance());
+  // Ba1.setPassword("newthinghaha");
+  //  if( Ba1.deposit(555.05) ){
+      //    System.out.println("Deposit Success!");
+    //  }else{
+      //     System.out.println("Deposit Failure");
+      //}
+    //  if( Ba1.withdraw(7777) ){
+    //         System.out.println("Withdrawal Success!");
+    //    }else{
+    //         System.out.println("Withdrawal Failure");
+  //      }
+//    System.out.println(Ba1.getBalance());
+   //System.out.println(Ba1.toString());
+//}
+//}
+public class Tester {
+    public static void main(String[] args) {
+        boolean allGood = true;
+        BankAccount x = new BankAccount(123456, "test123");
+
+        allGood = check("x.toString()", x.toString(), "123456\t0.0");
+        allGood = check("x.deposit(12)", x.deposit(12), true);
+        allGood = check("x.getBalance()", x.getBalance(), 12.0);
+        allGood = check("x.toString()", x.toString(), "123456\t12.0");
+        allGood = check("x.getAccountID", x.getAccountID(), 123456);
+        allGood = check("x.deposit(-12)", x.deposit(-12), false);
+        allGood = check("x.withdraw(5)", x.withdraw(5), true);
+        allGood = check("x.withdraw(-2)", x.withdraw(-2), false);
+        allGood = check("x.withhdraw(100)", x.withdraw(100), false);
+        allGood = check("x.getBalance()", x.getBalance(), 7.0);
+
+        // There's no way to check the password, so I'll use a try/catch to
+        // see if it fails somehow.
+        try {
+            x.setPassword("new!");
+        } catch (Exception e) {
+            allGood = false;
+            System.out.println("Failure on x.setPassword: Crashed.");
         }
-    System.out.println(Ba1.getBalance());
-  }
+
+        if (allGood) System.out.println("All good!");
+    }
+
+    public static boolean check(String name, String actual, String expected) {
+        if (actual.equals(expected)) return true;
+        else {
+            System.out.println("Failure on " + name + ": Expected \"" + expected + "\", got \"" + actual + "\".");
+            return false;
+        }
+    }
+
+    public static boolean check(String name, double actual, double expected) {
+        if (actual == expected) return true;
+        else {
+            System.out.println("Failure on " + name + ": Expected \"" + expected + "\", got \"" + actual + "\".");
+            return false;
+        }
+    }
+
+    public static boolean check(String name, int actual, int expected) {
+        if (actual == expected) return true;
+        else {
+            System.out.println("Failure on " + name + ": Expected \"" + expected + "\", got \"" + actual + "\".");
+            return false;
+        }
+    }
+
+    public static boolean check(String name, boolean actual, boolean expected) {
+        if (actual == expected) return true;
+        else {
+            System.out.println("Failure on " + name + ": Expected \"" + expected + "\", got \"" + actual + "\".");
+            return false;
+        }
+    }
 }
